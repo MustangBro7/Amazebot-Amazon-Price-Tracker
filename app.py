ff7ce42f15a146b7280f649cccdda10f0d865b7f
Abhinav Mohan <abhinavmohan12@gmail.com>
2024-02-19 23:30:36 +0530

    Cleaned up code

--- app.py
+++ app.py
@@ -24,9 +24,7 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 app = Flask(__name__)
 CORS(app)
 scheduler = APScheduler()
-# scheduler.init_app(app)
 
-# @scheduler.task('cron', hour=23, minute=10 , timezone='Asia/Kolkata')
 def scheduled_update():
     current_data = get_data()
     for i in current_data:
@@ -46,25 +44,13 @@ def bot():
     chrome_options.add_argument("--headless")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
-    # chrome_options.add_argument("user-data-dir=selenium")
     driver = webdriver.Chrome(options=chrome_options)
-    # stealth(driver,
-    #    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.105 Safari/537.36',
-    #    languages=["en-US", "en"],
-    #    vendor="Google Inc.",
-    #    platform="Win32",
-    #    webgl_vendor="Intel Inc.",
-    #    renderer="Intel Iris OpenGL Engine",
-    #    fix_hairline=True,
-    #    )
     driver.get(url)
     cookies = driver.get_cookies()
     for cookie in cookies:
         driver.add_cookie(cookie)
     print(cookies)
     driver.get(url)
-    # captcha = driver.find_element(By.CSS_SELECTOR, 'iframe[role=presentation]')
-    # driver.switch_to.frame(captcha)
 
     try:    
         captcha =  driver.find_element(By.XPATH, f"//h4[contains(text(), 'Enter the characters you see below')]") 
@@ -95,20 +81,13 @@ def bot():
 
 
     # print(source)
-    with open("D:\python projects\page.html" , "w" , encoding = 'utf-8') as file:
-        file.write(driver.page_source)
     source = driver.page_source
     soup = bs(source , 'html.parser')
     # print(soup)
     price = soup.findAll(string=re.compile("₹|Rs\.?"))
-    # price1 = soup.findAll(string = re.compile("displayPrice"))
-    # price1 = driver.find_element(By.CLASS_NAME , "a-price-whole")
     price1 = soup.find('span' , class_="a-price-whole")
     symbol = soup.find('span' , class_="a-price-symbol")
     print(price1)
-    # image_container = soup.find('div' , class_='a-image-container a-dynamic-image-container greyBackground')
-    # product_image = image_container.find('img')
-    # link = product_image.get('src')
     image_container = driver.find_element(By.CLASS_NAME , "imgTagWrapper")
     image_link = image_container.find_element(By.TAG_NAME , 'img')
     img_src = image_link.get_attribute('src')
@@ -116,10 +95,6 @@ def bot():
     name = soup.find('span' , id = "productTitle")
     product_name = name.text.strip()    
     if price1 :
-        # print(price1[0])
-        # data = json.loads(price1[0])
-        # # Extract the value associated with the key "displayPrice"
-        # display_price = data['desktop_buybox_group_1'][0]['displayPrice']
         display_price = symbol.text + price1.text
         print(display_price)
     else:
@@ -127,11 +102,7 @@ def bot():
         display_price = "Out of stock"
     driver.close()
     updated_data = update_data(display_price , product_name , url , img_src)
-    # print(json.dumps(updated_data[2]))
-    # full_data = get_data()
-    
 
-    # schedule.every().day.at("22:03").do(scheduled_update)
     return {"Price": display_price , "Name": product_name , "link":url , "img_link":img_src}
 
 
@@ -157,11 +128,3 @@ if __name__ == '__main__':
     scheduler.add_job(id = "job2" , func= scheduled_update , trigger = "cron", day_of_week = 'mon-sun' , hour= 21 , minute=00 )
     scheduler.start()  
     app.run(debug=True)
-    
-
-
-# def run_scheduled_update_in_thread():
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
-# schedule.run_pending()
